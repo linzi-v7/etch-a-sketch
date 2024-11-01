77585953c0b2bf3e862fd73ab3de82bc84d00558
linzi-v7 <farisos7777@gmail.com>
2024-11-01 11:20:00 +0200

    Allow user to enter grid size

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
-generateGrid(100);
+generateGrid(16); //default grid for initial load
+
+
+let input = document.querySelector("#grid-size-input")
+let generateButton = document.querySelector("#generate-grid-button")
+
+generateButton.addEventListener("click", function () { generateGrid(input.value) })
 
 function changeColor(item)
 {
@@ -9,13 +15,20 @@ function changeColor(item)
 //returns a value between 0 and 255
 function getRandomRGB()
 {
-
     return Math.floor(Math.random() * 256);
 }
 
 function generateGrid(size = 16)
 {
+    if (size <= 0 || size > 100)
+    {
+        alert("Invalid :( Please input a number between 1 and 100");
+        return;
+    }
+
     const container = document.querySelector(".container");
+
+    container.innerHTML = ""; //remove previous grid before generating new one
 
     for (let i = 0; i < size * size; i++)
     {
